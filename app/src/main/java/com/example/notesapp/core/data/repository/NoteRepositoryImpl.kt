802f264dfcc016632626c44e79fb79499f10076c
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-09-08 18:07:10 +0300

    data :  note repository impl

---
+++ app/src/main/java/com/example/notesapp/core/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.example.notesapp.core.data.repository
+
+import com.example.notesapp.core.data.local.NoteDao
+import com.example.notesapp.core.data.mapper.toNoteEntityForDelete
+import com.example.notesapp.core.data.mapper.toNoteEntityForInsert
+import com.example.notesapp.core.data.mapper.toNoteItem
+import com.example.notesapp.core.domain.model.Note
+import com.example.notesapp.core.domain.repository.NoteRepository
+import javax.inject.Inject
+
+class NoteRepositoryImpl @Inject constructor(
+    private val noteDao: NoteDao
+) : NoteRepository {
+    override suspend fun upsertNote(note: Note) =
+        noteDao.upsertNoteEntity(note.toNoteEntityForInsert())
+
+
+    override suspend fun deleteNote(note: Note) =
+        noteDao.deleteNoteEntity(note.toNoteEntityForDelete())
+
+    override suspend fun getAllNotes(): List<Note> =
+        noteDao.getAllNoteEntities().map { it.toNoteItem() }
+
+
+}
