d953496efae20d562486981e6ace525fdcf2e38f
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-09-08 18:06:07 +0300

    data : mapper

---
+++ app/src/main/java/com/example/notesapp/core/data/mapper/NoteMappers.kt
@@ -0,0 +1,35 @@
+package com.example.notesapp.core.data.mapper
+
+import com.example.notesapp.core.data.local.NoteEntity
+import com.example.notesapp.core.domain.model.Note
+
+fun Note.toNoteEntityForInsert(
+): NoteEntity {
+    return NoteEntity(
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+        dateAdded = dateAdded
+    )
+}
+
+fun Note.toNoteEntityForDelete(
+): NoteEntity {
+    return NoteEntity(
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+        dateAdded = dateAdded,
+        id = id
+    )
+}
+
+fun NoteEntity.toNoteItem(): Note {
+    return Note(
+        title = title,
+        description = description,
+        imageUrl = imageUrl,
+        dateAdded = dateAdded,
+        id = id ?: 0
+    )
+}
