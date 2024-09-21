2c0419d3ff9b5de3acb33a4ebdae32bb780b4d34
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-09-21 09:16:59 +0300

    testing : main Coroutine rule

---
+++ app/src/test/java/com/example/notesapp/MainCoroutineRule.kt
@@ -0,0 +1,29 @@
+package com.example.notesapp
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainCoroutineRule(
+    val dispatcher: TestDispatcher = StandardTestDispatcher()
+) : TestWatcher() {
+
+
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+
+    override fun failed(e: Throwable?, description: Description?) {
+        super.failed(e, description)
+        Dispatchers.resetMain()
+    }
+
+}
