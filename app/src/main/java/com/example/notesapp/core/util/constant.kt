e221da93afb566a42be4a0ebf62f764207d9bd16
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-09-22 23:17:36 +0300

    core : constant

--- app/src/main/java/com/example/notesapp/core/util/constant.kt
+++ app/src/main/java/com/example/notesapp/core/util/constant.kt
@@ -3,7 +3,7 @@ package com.example.notesapp.core.util
 object Constant {
 
     const val DATABASE_NAME = "note_db"
-    const val API_KEY = ""
+    const val API_KEY = "46041002-2cb077a8c6f561a19325b9144"
     const val BASE_URL = "https://pixabay.com"
 
 }
